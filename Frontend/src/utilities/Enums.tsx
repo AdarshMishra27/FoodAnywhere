162c3e264c289c401ed05c203fe0f042ed4c122e
Your Name <you@example.com>
2023-11-03 00:12:02 +0530

    enums added

---
+++ Frontend/src/utilities/Enums.tsx
@@ -0,0 +1,17 @@
+export enum MealType {
+        BREAKFAST = "Breakfast",
+        LUNCH = "Lunch",
+        DINNER = "Dinner"
+}
+
+export enum Cuisine {
+        AMERICAN = "American",
+        SEAFOOD = "Seafood",
+        INDIAN = "Indian",
+        CHINESE = "Chinese",
+        ITALIAN = "Italian",
+        MEXICAN = "Mexican",
+        SPANSISH = "Spanish",
+        ISRAELI = "Israeli ",
+        JAPANESE = "Japanese"
+}
